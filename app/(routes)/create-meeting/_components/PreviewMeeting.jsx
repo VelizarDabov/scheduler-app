c7165fb1e601ffcb75c749f9c9d252fb9f440171
Velizar Dabov <velizardabov@gmail.com>
2024-03-29 16:38:06 +0200

    time interval calculations

---
+++ app/(routes)/create-meeting/_components/PreviewMeeting.jsx
@@ -0,0 +1,66 @@
+'use client'
+import { Calendar } from '@/components/ui/calendar'
+import { format, formatDate } from 'date-fns'
+import { Clock, MapPin } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
+const PreviewMeeting = ({formData}) => {
+    const [date, setDate]=useState(new Date());
+
+    useEffect(() => {
+        formData?.duration&&createTimeSlot(formData?.duration)
+    },[formData])
+
+    //time interval calculation
+    const createTimeSlot=(interval)=>{
+        const startTime = 8 * 60 //8 AM in min 
+        const endTime=22 * 60 //10 PM in min
+        const totalSlots=(endTime-startTime)/interval;
+        const slots = Array.from({length:totalSlots}, (_,i)=> {
+            const totalMinutes=startTime + i * interval;
+            const hours=Math.floor(totalMinutes / 60);
+            const minutes = totalMinutes % 60
+            const formattedHours=hours > 12 ? hours - 12 : hours;
+            const period=hours >= 12 ? 'PM': 'AM'
+            return `${String(formattedHours).padStart(2,'0')}:${String(minutes).padStart(2,'0')}:${period}`
+        })
+  console.log(slots)
+    }
+  return (
+    <div className='p-5 py-10 shadow-lg m-5 border-t-8'>
+       <Image src='/logo.svg' width={150} height={150} alt='logo'/>
+       <div className='grid grid-cols-1 md:grid-cols-3 mt-5'>
+        {/* Meeting info */}
+        <div>
+<h2 className='p-4 border-r'>Business Name</h2>
+<h2 className='font-bold text-2xl'>{formData?.eventName?formData?.eventName:'Meeting Name'}</h2>
+<div className='mt-5 flex flex-col gap-4'>
+    <h2 className='flex gap-2'><Clock />{formData?.duration} Min</h2>
+<h2 className='flex gap-2'><MapPin />{formData?.locationType} Meeting</h2>
+{formData.locationUrl &&(<Link className='text-primary' href={formData?.locationUrl}>{formData?.locationUrl}</Link>)}
+
+</div>
+        </div>
+        {/* time and date selection */}
+        <div className='md:cols-span-2 flex px-4'>
+       <div className='flex flex-col'>
+        <h2 className='font-bold text-lg'>Select Date & time</h2>
+       <Calendar
+     mode="single"
+     selected={date}
+     onSelect={setDate}
+     className="rounded-md border mt-5"
+   />
+ 
+       </div>
+  
+        </div>
+       </div>
+    </div>
+  )
+}
+
+export default PreviewMeeting
+
--- app/(routes)/create-meeting/page.jsx
+++ app/(routes)/create-meeting/page.jsx
@@ -1,18 +1,19 @@
 'use client'
 import React, { useState } from 'react'
 import MeetingForm from './_components/MeetingForm'
+import PreviewMeeting from './_components/PreviewMeeting'
 
 const CreateMeeting = () => {
-    const [formData, setFormData]=useState()
+    const [formData, setFormData]=useState({})
   return (
     <div className='grid grid-cols-1 md:grid-cols-3'>
         {/* Meeting form */}
         <div className='shadow-md border h-screen'>
-<MeetingForm setFormValue={(e)=> setFormData(e)}/>
+<MeetingForm setFormValue={(e)=>  setFormData(e)}/>
         </div>
 {/* Preview */}
-<div className='hidden md:col-span-2 '>
-
+<div className='md:col-span-2'>
+<PreviewMeeting formData={formData}/>
 </div>
     </div>
   )
