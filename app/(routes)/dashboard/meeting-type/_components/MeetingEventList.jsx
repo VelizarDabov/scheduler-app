45eb25d3c4604ee962b30ac34e6acc55a1e2a85a
Velizar Dabov <velizardabov@gmail.com>
2024-03-30 09:54:42 +0200

    delete appointment functionality

--- app/(routes)/dashboard/meeting-type/_components/MeetingEventList.jsx
+++ app/(routes)/dashboard/meeting-type/_components/MeetingEventList.jsx
@@ -1,50 +1,127 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { app } from "@/config/Firebase";
 import { useUser } from "@clerk/nextjs";
 import {
   collection,
+  deleteDoc,
+  doc,
   getDocs,
   getFirestore,
+  orderBy,
   query,
   where,
 } from "firebase/firestore";
-import { Clock, MapPin } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Clock, Copy, MapPin, Pen, Settings, Trash } from "lucide-react";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const MeetingEventList = () => {
   const db = getFirestore(app);
   const { user } = useUser();
   const [eventList, setEventList] = useState([]);
+
   useEffect(() => {
     user && getEventList();
   }, [user]);
   const getEventList = async () => {
+    setEventList([]);
     const q = query(
       collection(db, "MeetingEvent"),
-      where("createdBy", "==", user?.primaryEmailAddress.emailAddress)
+      where("createdBy", "==", user?.primaryEmailAddress.emailAddress),
+      orderBy("id", "desc")
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       setEventList((prev) => [...prev, doc.data()]);
     });
   };
+  const onDeleteEvent = async (event) => {
+    if (!event || !event.id) {
+      console.error("Event or event ID is undefined");
+      return;
+    }
 
+    try {
+      await deleteDoc(doc(db, "MeetingEvent", event.id));
+      getEventList();
+      toast("Event Deleted!");
+    } catch (error) {
+      console.error("Error deleting event:", error);
+      // Handle the error appropriately, e.g., show an error message
+    }
+  };
   return (
     <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-      {eventList?.map((event, index) => (
-        <div key={index} className="border shadow-md border-t-8 rounded-lg p-5 flex flex-col gap-3" >
-          <h2 className="font-medium text-2xl">{event?.eventName}</h2>
-          <div className="flex justify-between">
-            <h2>
-              <Clock />
-              {event.duration}
-            </h2>
-            <h2>
-                <MapPin/>{event.locationType}
-            </h2>
+      {eventList.length > 0 ? (
+        eventList?.map((event, index) => (
+          <div
+            key={index}
+            className="border shadow-md border-t-8 rounded-lg p-5 flex flex-col gap-3"
+            style={{ borderTopColor: event?.themeColor }}
+          >
+            <div className="flex justify-end">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Settings className="cursor-pointer" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="flex gap-2">
+                    <Pen />
+                    Edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="flex gap-2"
+                    onClick={() => onDeleteEvent(event)}
+                  >
+                    <Trash />
+                    Delete
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <h2 className="font-medium text-2xl">{event?.eventName}</h2>
+            <div className="flex justify-between">
+              <h2 className="flex gap-2 text-gray-500">
+                <Clock />
+                {event.duration}
+              </h2>
+              <h2 className="flex gap-2 text-gray-500">
+                <MapPin />
+                {event.locationType}
+              </h2>
+            </div>
+            <hr></hr>
+            <div className="flex justify-between text-primary">
+              <h2
+                className="flex items-center text-primary gap-2  text-sm cursor-pointer "
+                onClick={() => {
+                  navigator.clipboard.writeText(event.locationUrl);
+                  toast("Copied to Clickboard");
+                }}
+              >
+                <Copy className="h-4 w-4 " /> Copy Link
+              </h2>
+              <Button
+                variant="outline"
+                className="border-primary rounded-full text-primary text-sm"
+              >
+                Share
+              </Button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 };
