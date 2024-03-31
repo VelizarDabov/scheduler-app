74604ad7bd93ec7fb38120bd49ba3c3cf5c9d50f
Velizar Dabov <velizardabov@gmail.com>
2024-03-31 08:49:17 +0200

    add meeting

--- app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
+++ app/(routes)/[business]/_components/MeetingTimeDateSelection.jsx
@@ -1,106 +1,159 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { format, formatDate } from "date-fns";
-import { CalendarCheck, Clock, LoaderIcon, MapPin, TimerIcon } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import TimeDateSelection from "./TimeDateSelection";
-import UserFormInfo from "./UserFormInfo";
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { format } from 'date-fns'
+import { CalendarCheck, Clock, LoaderIcon, MapPin, Timer } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import TimeDateSelection from './TimeDateSelection'
+import UserFormInfo from './UserFormInfo'
+import { QuerySnapshot, collection, doc, getDocs, getFirestore, query, setDoc, where } from 'firebase/firestore'
 
-const MeetingTimeDateSelection = ({ eventInfo, businessInfo }) => {
-  const [date, setDate] = useState(new Date());
-  const [timeSlots, setTimeSlots] = useState();
-  const [enableTimesSlot, setEnableTimesSlot] = useState(false);
-  const [selectedTime, setSelectedTime] = useState();
-  const [step, setStep] = useState(1);
-  const [loading,setLoading]=useState(false);
-  useEffect(() => {
-    eventInfo?.duration && createTimeSlot(eventInfo?.duration);
-  }, [eventInfo]);
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
+import { app } from '@/config/Firebase'
 
-  //time interval calculation
-  const createTimeSlot = (interval) => {
-    const startTime = 8 * 60; //8 AM in min
-    const endTime = 22 * 60; //10 PM in min
-    const totalSlots = (endTime - startTime) / interval;
-    const slots = Array.from({ length: totalSlots }, (_, i) => {
+
+
+function MeetingTimeDateSelection({eventInfo,businessInfo}) {
+    const [date,setDate]=useState(new Date())
+    const [timeSlots,setTimeSlots]=useState();
+    const [enableTimeSlot,setEnabledTimeSlot]=useState(false);
+    const [selectedTime,setSelectedTime]=useState();
+    const [userName,setUserName]=useState();
+    const [userEmail,setUserEmail]=useState();
+    const [userNote,setUserNote]=useState('');
+    const [prevBooking,setPrevBooking]=useState([]);
+    const [step,setStep]=useState(1);
+    const router=useRouter();
+    const db=getFirestore(app);
+    const [loading,setLoading]=useState(false);
+ 
+    useEffect(()=>{
+        eventInfo?.duration&&createTimeSlot(eventInfo?.duration)
+    },[eventInfo])
+    const createTimeSlot=(interval)=>{
+        const startTime = 8 * 60; // 8 AM in minutes
+        const endTime = 22 * 60; // 10 PM in minutes
+        const totalSlots = (endTime - startTime) / interval;
+        const slots = Array.from({ length: totalSlots }, (_, i) => {
       const totalMinutes = startTime + i * interval;
       const hours = Math.floor(totalMinutes / 60);
       const minutes = totalMinutes % 60;
-      const formattedHours = hours > 12 ? hours - 12 : hours;
-      const period = hours >= 12 ? "PM" : "AM";
-      return `${String(formattedHours).padStart(2, "0")}:${String(
-        minutes
-      ).padStart(2, "0")}:${period}`;
+      const formattedHours = hours > 12 ? hours - 12 : hours; // Convert to 12-hour format
+      const period = hours >= 12 ? 'PM' : 'AM';
+      return `${String(formattedHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')} ${period}`;
     });
-    setTimeSlots(slots);
-  };
-  const handleDateChange = (date) => {
-    setDate(date);
-    const day = format(date, "EEEE");
-    if (businessInfo.daysAvailable?.[day]) {
-      setEnableTimesSlot(true);
-    } else {
-      setEnableTimesSlot(false);
+ 
+    setTimeSlots(slots); 
     }
-  };
-  return (
-    <div
-      className="p-5 py-10 shadow-lg m-5 border-t-8 mx-10 md:mx-8 lg:mx-20 my-10"
-      style={{ borderTopColor: eventInfo?.themeColor }}
-    >
-      <Image src="/logo.svg" width={150} height={150} alt="logo" />
-      <div className="grid grid-cols-1 md:grid-cols-3 mt-5">
-        {/* Meeting info */}
-        <div>
-          <h2 className="p-4 border-r">{businessInfo?.businessName}</h2>
-          <h2 className="font-bold text-2xl">
-            {eventInfo?.eventName ? eventInfo?.eventName : "Meeting Name"}
-          </h2>
-          <div className="mt-5 flex flex-col gap-4">
-            <h2 className="flex gap-2">
-              <Clock />
-              {eventInfo?.duration} Min
-            </h2>
-            <h2 className="flex gap-2">
-              <MapPin />
-              {eventInfo?.locationType} Meeting
-            </h2>
-            <h2 className="flex gap-2">
-              <CalendarCheck />
-              {format(date, "PPP")}
-            </h2>
-            {selectedTime && (
-              <h2 className="flex gap-2">
-                <TimerIcon />
-                {selectedTime}
-              </h2>
-            )}
 
-            {eventInfo.locationUrl && (
-              <Link className="text-primary" href={eventInfo?.locationUrl}>
-                {eventInfo?.locationUrl}
-              </Link>
-            )}
-          </div>
-        </div>
-        {/* time and date selection */}
-        {step == 1 ? (
-          <TimeDateSelection
+  
+    const handleDateChange=(date)=>{
+        setDate(date);
+        const day=format(date,'EEEE');
+        if(businessInfo?.daysAvailable?.[day])
+        {
+            getEventBooking(date)
+            setEnabledTimeSlot(true)
+        }
+        else{
+          
+            setEnabledTimeSlot(false)
+        }
+    }
+
+  
+    const handleScheduleEvent=async()=>{
+
+            const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+            if(regex.test(userEmail)==false)
+            {
+                toast('Enter valid email address')
+                return ;
+            }
+        const docId=Date.now().toString();
+            setLoading(true)
+        await setDoc(doc(db,'ScheduledMeetings',docId),{
+            businessName:businessInfo.businessName,
+            businessEmail:businessInfo.email,
+            selectedTime:selectedTime,
+            selectedDate:date,
+            formatedDate:format(date,'PPP'),
+            formatedTimeStamp:format(date,'t'),
+            duration:eventInfo.duration,
+            locationUrl:eventInfo.locationUrl,
+            eventId:eventInfo.id,
+            id:docId,
+            userName:userName,
+            userEmail:userEmail,
+            userNote:userNote
+        }).then(resp=>{
+            toast('Meeting Scheduled successfully!');
+            setLoading(false)
+        })
+    }
+
+   
+
+   const getEventBooking =async(date_)=> {
+    const q=query(collection(db, 'ScheduledMeetings'),where('selectedDate', '==',date_ ), where('eventId', '==', eventInfo?.id))
+    const querySnapshot=await getDocs(q)
+
+    querySnapshot.forEach((doc)=>{
+        console.log('--', doc.data())
+        setPrevBooking(prev=>[...prev, doc.data()])
+    })
+   }
+ 
+  return (
+    <div className='p-5 py-10 shadow-lg m-5 border-t-8
+    mx-10
+    md:mx-26
+    lg:mx-56
+    my-10'
+    style={{borderTopColor:eventInfo?.themeColor}}
+    >
+       <Image src='/logo.svg' alt='logo'
+       width={150}
+       height={150}/>
+       <div className='grid grid-cols-1 md:grid-cols-3 mt-5'>
+            {/* Meeting Info  */}
+            <div className='p-4 border-r'>
+                <h2>{businessInfo?.businessName}</h2>
+                <h2
+                className='font-bold text-3xl'
+                >{eventInfo?.eventName?eventInfo?.eventName:'Meeting Name'}</h2>
+                <div className='mt-5 flex flex-col gap-4'>
+                    <h2 className='flex gap-2'><Clock/>{eventInfo?.duration} Min </h2>
+                    <h2 className='flex gap-2'><MapPin/>{eventInfo?.locationType} Meeting </h2>
+                    <h2 className='flex gap-2'><CalendarCheck/>{format(date,'PPP')}  </h2>
+                  {selectedTime&&  <h2 className='flex gap-2'><Timer/>{selectedTime}  </h2>}
+                  
+                    <Link href={eventInfo?.locationUrl?eventInfo?.locationUrl:'#'}
+                    className='text-primary'
+                    >{eventInfo?.locationUrl}</Link>
+                </div>
+            </div>
+            {/* Time & Date Selction  */}
+          {step==1? <TimeDateSelection
             date={date}
-            enableTimesSlot={enableTimesSlot}
+            enableTimeSlot={enableTimeSlot}
             handleDateChange={handleDateChange}
             setSelectedTime={setSelectedTime}
-            selectTime={selectedTime}
             timeSlots={timeSlots}
-          />
-        ) : (
-          <UserFormInfo />
-        )}
-      </div>
-      <div className='flex gap-3 justify-end'>
+            selectedTime={selectedTime}
+            prevBooking={prevBooking}
+           />:
+           <UserFormInfo
+            setUserName={setUserName}
+            setUserEmail={setUserEmail}
+            setUserNote={setUserNote}
+           />}
+     
+
+       </div>
+       <div className='flex gap-3 justify-end'>
         {step==2&&<Button variant="outline" 
         onClick={()=>setStep(1)}>Back</Button>}
       {step==1? <Button className="mt-10 float-right"
@@ -108,13 +161,14 @@ const MeetingTimeDateSelection = ({ eventInfo, businessInfo }) => {
         onClick={()=>setStep(step+1)}
        >Next
        </Button>:
-       <Button  
-      
+       <Button disabled={!userEmail||!userName} 
+       onClick={handleScheduleEvent}
        > 
        {loading?<LoaderIcon className='animate-spin'/>:'Schedule' }
       </Button>}
        </div>
     </div>
-  );
-};
-export default MeetingTimeDateSelection;
+  )
+}
+
+export default MeetingTimeDateSelection
--- app/(routes)/[business]/_components/UserFormInfo.jsx
+++ app/(routes)/[business]/_components/UserFormInfo.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import React from "react";
 
-const UserFormInfo = () => {
+const UserFormInfo = ({setUserEmail,setUserName,setUserNote}) => {
   return (
-    <div>UserFormInfo</div>
-  )
-}
+    <div className="p-4 px-8 flex flex-col gap-3">
+      <h2 className="font-bold text-xl">Enter Details</h2>
+      <div>
+        <h2>Name*</h2>
+        <Input onChange={(e) => setUserName(e.target.value)} />
+      </div>
+      <div>
+        <h2>Email*</h2>
+        <Input onChange={(e) => setUserEmail(e.target.value)} />
+      </div>
+      <div>
+        <h2>Share Any Notes</h2>
+        <Input onChange={(e) => setUserNote(e.target.value)} />
+      </div>
+      <div>
+        <h2 className="text-xs text-gray-400">
+          By Proceeding, you confirm that you read and agree VND terms and
+          conditions
+        </h2>
+      </div>
+    </div>
+  );
+};
 
-export default UserFormInfo
+export default UserFormInfo;
