2492f863e134bef34e4ea3e5431e4c79eb587096
Velizar Dabov <velizardabov@gmail.com>
2024-04-01 23:46:40 +0300

    schedular page

--- app/(routes)/dashboard/scheduled-meeting/page.jsx
+++ app/(routes)/dashboard/scheduled-meeting/page.jsx
@@ -1,52 +1,71 @@
-'use client'
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ScheduledMeetingList from './_components/ScheduledMeetingList'
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
-import { app } from '@/config/Firebase'
-import { useUser } from "@clerk/nextjs";
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { format } from 'date-fns'
+import { collection, getDocs, getFirestore, query, where,  } from 'firebase/firestore'
 
-const ScheduledMeetings = () => {
-    const db = getFirestore(app)
-    const { user } = useUser();
+
+import { format } from 'date-fns'
+import { useUser } from '@clerk/nextjs'
+import { app } from '@/config/Firebase'
+
+function ScheduledMeeting() {
+
+    const db=getFirestore(app);
+    const {user}=useUser()
     const userEmail = user?.primaryEmailAddress.emailAddress;
-const [meetingList, setMeetingList]=useState([])
-    useEffect(()=> {
-user&&getScheduledMeetings()
+    const [meetingList,setMeetingList]=useState([]);
+    useEffect(()=>{
+        user&&getScheduledMeetings();
     },[user])
-    const getScheduledMeetings=async()=> {
-        const q = query(collection(db, 'ScheduledMeetings'), where('businessEmail', '==',userEmail))
-        const querySnapshot=await getDocs(q)
-        querySnapshot.forEach(doc=> {
-           setMeetingList(prev=>[...prev, doc.data()])
+  
+    const getScheduledMeetings=async()=>{
+        setMeetingList([])
+        const q=query(collection(db,'ScheduledMeetings'),
+        where('businessEmail','==',userEmail));
+        const querySnapshot=await getDocs(q);
+
+        querySnapshot.forEach(doc=>{
+            console.log(doc.data());
+            setMeetingList(prev=>[...prev,doc.data()])
         })
+
     }
-    const filterMeetingList=(type)=> {
-        if(type=='upcoming'){
-           return meetingList.filter(item=>item.formatedTimeStamp >= format(new Date(), 't'))
+
+  
+    const filterMeetingList=(type)=>{
+        if(type=='upcoming')
+        {
+            return meetingList.filter(item=>item.formatedTimeStamp>=format(new Date(),'t'));
+        }
+        else{
+            return meetingList.filter(item=>item.formatedTimeStamp<format(new Date(),'t'));
+
         }
     }
+
   return (
     <div className='p-10'>
-        <h2 className='font-bold text-2xl'>ScheduledMeetings</h2>
+        <h2 className='font-bold text-2xl'>Scheduled Meetings</h2>
         <hr className='my-5'></hr>
-        <Tabs defaultValue="upcomming" className="w-[400px]">
-  <TabsList>
-    <TabsTrigger value="upcomming">Upcomming</TabsTrigger>
-    <TabsTrigger value="expired">Expired</TabsTrigger>
-  </TabsList>
-  <TabsContent value="upcomming">
-    <ScheduledMeetingList meetingList={filterMeetingList('upcoming')} />
-    
-  </TabsContent>
-  <TabsContent value="expired">Change your expired here.</TabsContent>
-</Tabs>
+        <Tabs defaultValue="upcoming" className="w-[400px]">
+        <TabsList>
+            <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
+            <TabsTrigger value="expired">Expired</TabsTrigger>
+        </TabsList>
+        <TabsContent value="upcoming">
+            <ScheduledMeetingList
+            meetingList={filterMeetingList('upcoming')}
+            /> </TabsContent>
+        <TabsContent value="expired">
+        <ScheduledMeetingList
+            meetingList={filterMeetingList('expired')}
+            /> 
+        </TabsContent>
+        </Tabs>
 
     </div>
   )
 }
 
-export default ScheduledMeetings
+export default ScheduledMeeting
